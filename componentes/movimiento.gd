cae3598a4a02a44a73d77ba5e7d6ad906477e70d
ReyCristian <Rey.cristian.eze@gmail.com>
2025-05-11 16:23:00 -0300

    diagrama de clase / objetivo y deidad

--- componentes/movimiento.gd
+++ componentes/movimiento.gd
@@ -1,5 +1,15 @@
-class_name Movimiento
 extends Resource
+class_name Movimiento
+
+@export var destino: Objetivo
+@export var teclas: Array[String] = ["ui_left", "ui_right", "ui_up", "ui_down"]
 
 func mover(personaje: CharacterBody2D, delta: float) -> void:
-	pass  # A implementar por herencias
+	var input_vector = Input.get_vector(teclas[0], teclas[1], teclas[2], teclas[3])
+
+	if input_vector == Vector2.ZERO and destino:
+		var origen = personaje.global_position
+		input_vector = destino.obtener_direccion(origen)
+
+	personaje.velocity = input_vector * personaje.get("speed")
+	personaje.move_and_slide()
--- componentes/objetivo.gd
+++ componentes/objetivo.gd
@@ -3,41 +3,46 @@ class_name Objetivo
 
 var personaje: Personaje
 var obstaculo: Obstaculo
-var posicion: Vector2
-var direccion: Vector2
+var posicion: Vector2 = Vector2.INF
+var direccion: Vector2 = Vector2.INF
 
 func agregar_personaje(p: Personaje):
 	personaje = p
-	#posicion = Vector2.ZERO
+	#posicion = Vector2.INF
 	#obstaculo = null
-	#direccion = Vector2.ZERO
+	#direccion = Vector2.INF
 
 func agregar_obstaculo(o: Obstaculo):
 	obstaculo = o
-	#posicion = Vector2.ZERO
+	#posicion = Vector2.INF
 	#personaje = null
-	#direccion = Vector2.ZERO
+	#direccion = Vector2.INF
 
 func agregar_posicion(x: float, y: float):
 	posicion = Vector2(x, y)
 	#personaje = null
 	#obstaculo = null
-	#direccion = Vector2.ZERO
+	#direccion = Vector2.INF
 
 func agregar_direccion(dir: Vector2):
 	direccion = dir
 
 func obtener_posicion() -> Vector2:
-	if posicion != Vector2.ZERO:
+	if posicion != Vector2.INF:
 		return posicion
 	elif personaje:
 		return personaje.global_position
 	elif obstaculo:
 		return obstaculo.global_position
 	else:
-		return Vector2.ZERO
+		return Vector2.INF
 
 func obtener_direccion(desde: Vector2) -> Vector2:
-	if direccion != Vector2.ZERO:
+	if direccion != Vector2.INF:
 		return direccion.normalized()
-	return (obtener_posicion() - desde).normalized()
+
+	var destino = obtener_posicion()
+	if destino == Vector2.INF:
+		return Vector2.ZERO
+
+	return (destino - desde).normalized()
