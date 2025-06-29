0133c58088bf01bce15d0a43f9c0a18a5128ae00
Marianela Sanhueza <exemacaya22@gmail.com>
2025-06-28 23:15:30 -0300

    sonidos
    música fondo, menús

--- menu/menu_principal.gd
+++ menu/menu_principal.gd
@@ -8,12 +8,12 @@ func _ready() -> void:
 
 
 func _on_inicio_pressed() -> void: #inicio el juego
-	#$Menu.stream_paused = true
+	$VBoxContainer/Menu.stream_paused = true
 	get_tree().change_scene_to_file("res://escenas/node_2d.tscn")  
 
 func _on_salida_pressed() -> void:
 	get_tree().quit()  # Cierro el juego
 
 func _on_opciones_pressed() -> void:
-	#$Menu.stream_paused = true
+	$VBoxContainer/Menu.stream_paused = true
 	get_tree().change_scene_to_file("res://menu/menu_opciones.tscn")  # Cargo el menú de opciones
